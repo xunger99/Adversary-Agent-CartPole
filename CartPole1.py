d20063c08da5f59d24566f47c8293722d062f769
xunger99 <60506482+xunger99@users.noreply.github.com>
2021-10-08 13:41:45 +0800

    Add files via upload

--- CartPole1.py
+++ CartPole1.py
@@ -1,8 +1,11 @@
-# Xun's test code
+# Xun's test code, show how to fit two agents simultaneously for a simple env with two isolated cart-pole systems. 
+#
+# Set-ups:  Model: simple, 4 inputs, 2cp env with ks=20, reward: win+1, lose-100. Fit two agents together. 
+#
 # Hint: Use the env with Tensorflow >2.0
-# I use this code to test the new env with 2 cart-pole systems. 
+#
 #               by Professor Xun Huang @ Peking University, 2021. 
-
+#
 
 import gym
 
@@ -14,8 +17,8 @@ from copy import deepcopy
 
 # Define learning policy
 #import rl 
-from agent import SARSAAgent_Xun
-#from rl.agents import SARSAAgent, DQNAgent, DDPGAgent
+from Xun_agent import SARSAAgent_Xun
+from rl.agents import SARSAAgent, DQNAgent, DDPGAgent
 from rl.policy import EpsGreedyQPolicy, BoltzmannQPolicy
 from rl.memory import SequentialMemory
 from rl.core import Processor
@@ -23,41 +26,20 @@ from rl.core import Processor
 from keras import Input
 import random
 import numpy as np
-from tensorflow import keras
-#import keras
-from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import Adam
+#from tensorflow import keras
+import keras
+from keras.layers import Dense, Flatten
+from keras.models import Sequential
+#from tensorflow.keras.optimizers import Adam
 
 
-
-
-"""
-# Parallel environments
-env = make_vec_env('XunCartPole-v0', n_envs=1)
-
-pdb.set_trace()
-model = A2C(MlpPolicy, env, verbose=1)
-model.learn(total_timesteps=25000)
-model.save("a2c_cartpole")
-
-del model # remove to demonstrate saving and loading
-
-model = A2C.load("a2c_cartpole")
-
-obs = env.reset()
-while True:
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
-    env.render()
-"""
-
-
-
-
-
-
-env = gym.make('CartPole-v1') #CartPole-v1') #XunCartPole-v0')    
+# modify the name to XunCartPole-v0, XunCartPole-v1, XunCartPole-v2, to XunCartPole-v4
+# XunCartPole-v0, no spring
+# XunCartPole-v1, reward loss: -100, win: 1 
+# XunCartPole-v2, 
+# XunCartPole-v3, 
+# XunCartPole-v4, 
+env = gym.make('XunCartPole-v1') 
 
 # print states
 states = env.observation_space.shape[0]
@@ -66,27 +48,6 @@ print('States', states)
 # print actions
 actions = env.action_space.n
 print('Actions', actions)
-
-# Random control performance? 
-episodes = 0
-for episode in range(1,episodes+1):
-    # At each begining reset the game 
-    state = env.reset()
-    # set done to False
-    done = False
-    # set score to 0
-    score = 0
-    # while the game is not finished
-    while not done:  # When done= True, the game is lost  
-        # visualize each step
-        env.render()
-        # choose a random action
-        action = random.choice([0,1])
-        # execute the action
-        n_state, reward, done, info = env.step(action)
-        # keep track of rewards
-        score+=reward
-    print('episode {} score {}'.format(episode, score))
 
 
 # Define a smart agent (a very small and simple network)
@@ -102,105 +63,226 @@ def agent_NN(states, actions):
 
 # Define the model for one cart-pole system
 obs_shape = 4       # observations: x, x_dot, theta, theta_dot
+full_obs_shape = 8 
 ctr_shape = 2       # control: +force, -force
+#model1 = agent_NN(obs_shape, ctr_shape)
 model1 = agent_NN(obs_shape, ctr_shape)
+model2 = agent_NN(obs_shape, ctr_shape)
 model1.summary()
-
-
-
-
 
 
 
 #################################################################################
 # Define agent type 
 # 10000 can be changed in the core.py, log_interval
-agent = 1   # only 1 is working for now.  0: dqn (not working anymore because of incompatible version), 1: sarsa, 2: ddpg
-if agent==0:
-    # all parameters are from https://github.com/keras-rl/keras-rl/blob/master/examples/dqn_cartpole.py    
-    memory = SequentialMemory(limit=50000, window_length=1)  
-    policy = BoltzmannQPolicy()
-    pdb.set_trace()
-    agent = DQNAgent(model = model1, memory = memory, policy = policy, nb_actions = env.action_space.n, nb_steps_warmup=10)
+policy = EpsGreedyQPolicy()     # from the original tutorial code
+agent1 = SARSAAgent_Xun(model = model1, policy = policy, nb_actions = env.action_space.n)
+agent2 = SARSAAgent_Xun(model = model2, policy = policy, nb_actions = env.action_space.n)
 
-elif agent==1:
-    policy = EpsGreedyQPolicy()     # from the original tutorial code
-    agent1 = SARSAAgent_Xun(model = model1, policy = policy, nb_actions = env.action_space.n)
-    agent2 = SARSAAgent_Xun(model = model1, policy = policy, nb_actions = env.action_space.n)
-elif agent==2:
-    pass
-    
-else:
-    print('incorrect agent?!?')
     
 # Agent and compile and training
 agent1.compile('adam', metrics = ['mse'])
 agent2.compile('adam', metrics = ['mse'])
 
-# only for tests
-agent1.load_weights('sarsa_weights.h5f')
-agent2.load_weights('sarsa_weights.h5f')
-env=gym.make('XunCartPole-v0')
 env.reset()
-pdb.set_trace()
-#env.step(action)
-#_ = agent.test(env, nb_episodes = 2, visualize= True)
-
-episodes = 20
-for episode in range(1,episodes+1):
-    #pdb.set_trace()    
-    # At each begining reset the game 
-    obs = deepcopy(env.reset()) 
-    # set done to False
-    done = False
-    # set score to 0
-    score = 0
-    # while the game is not finished
-    #pdb.set_trace()
-    while not done:  # When done= True, the game is lost  
-        obs1= obs[0:4]  # The first 4 for sys 1
-        obs2= obs[4:]   # The second 4 for sys 2
-
-        # choose actions from the agent
-        action1 = agent1.forward(obs1)
-        action2 = agent2.forward(obs2)
-        actions = [action1, action2] 
-        #actions = action1
-        # execute the action
-        #
-        obs, reward, done, info = env.step(actions)
-        # keep track of rewards
-        env.render()
-        score+=reward
-        
-        #print('actions {}'.format(actions))
-    #pdb.set_trace()    
-    print('episode {} score {}'.format(episode, score))
-
-env.close()
-
-
-
-
-
-pdb.set_trace()
-
-
-agent.fit(env, nb_steps = 30001, visualize = False, verbose = 1)
-#model1.summary()
-
-# Then, test the trained model
-scores = agent.test(env, nb_episodes = 10, visualize=True)
-env.env.close()
-
-# Next, save the model 
-agent.save_weights('sarsa_weights.h5f', overwrite=True)
-
-
-pdb.set_trace()
-# load the weights
-# agent.load_weights('sarsa_weights.h5f')
-env = gym.make('CartPole-v1')           #env=gym.make('XunCartPole-v0')
-_ = agent.test(env, nb_episodes = 10, visualize= True)
 #pdb.set_trace()
+
+
+
+###############################
+##  Fit, prepared for two agents! 
+###############################
+import collections
+import numpy as np
+from keras.callbacks import History
+from keras.models import Model
+from keras.layers import Input, Lambda
+import keras.backend as K
+
+from rl.core import Agent
+from rl.agents import SARSAAgent
+from rl.agents.dqn import mean_q
+from rl.util import huber_loss
+from rl.policy import EpsGreedyQPolicy, GreedyQPolicy
+from rl.util import get_object_config
+from copy import deepcopy
+import pdb
+from rl.callbacks import (
+    CallbackList,
+    TestLogger,
+    TrainEpisodeLogger,
+    TrainIntervalLogger,
+    Visualizer
+)
+# Extensively simplified fit for 2 agents
+def Xun_fit2(agent1, agent2, env, nb_steps, action_repetition=1, callbacks=None, verbose=1,
+            nb_max_start_steps=0, log_interval=10000,
+            nb_max_episode_steps=None):
+    """Trains the agent on the given environment.
+    """
+    print("The fit function from Xun 20201")
+    agent1.training = True
+    agent2.training = True
+    callbacks1 = [] if not callbacks else callbacks[:]
+    callbacks1 += [TrainIntervalLogger(interval=log_interval)]
+    callbacks2 = [] if not callbacks else callbacks[:]
+    callbacks2 += [TrainIntervalLogger(interval=log_interval)]    
+    
+
+    history1 = History()
+    callbacks1 += [history1]
+    callbacks1 = CallbackList(callbacks1)
+    history2 = History()
+    callbacks2 += [history2]
+    callbacks2 = CallbackList(callbacks2)    
+    
+    
+    #pdb.set_trace()             
+    callbacks1.set_model(agent1)  
+    callbacks1._set_env(env) 
+    callbacks2.set_model(agent2)  
+    callbacks2._set_env(env)         
+        
+    params = {'nb_steps': nb_steps,}
+    if hasattr(callbacks1, 'set_params'):
+        callbacks1.set_params(params)
+    else:
+        callbacks1._set_params(params)
+    if hasattr(callbacks2, 'set_params'):
+        callbacks2.set_params(params)
+    else:
+        callbacks2._set_params(params)        
+        
+    #self._on_train_begin()
+    callbacks1.on_train_begin()      # cannot remove. xun
+    callbacks2.on_train_begin()     
+    
+    
+    episode = np.int16(0)
+    agent1.step = np.int16(0)
+    agent2.step = np.int16(0)
+    observation = None
+    episode_reward1 = None
+    episode_reward2 = None    
+    episode_step = None
+    did_abort = False
+        
+    try:
+        while agent1.step < nb_steps:
+            if observation is None:  # start of a new episode            
+                callbacks1.on_episode_begin(episode)
+                callbacks2.on_episode_begin(episode)
+                
+                episode_step = np.int16(0)
+                episode_reward1 = np.float32(0)
+                episode_reward2 = np.float32(0)
+                
+                # Obtain the initial observation by resetting the environment.
+                agent1.reset_states()
+                agent2.reset_states()
+                observation = deepcopy(env.reset())
+                #assert observation is not None
+
+            # Run a single step.
+            callbacks1.on_step_begin(episode_step)   # cannot remove. xun
+            callbacks2.on_step_begin(episode_step)
+            
+            obs1= observation[0:4]
+            obs2= observation[4:]
+            
+            action1 = agent1.forward(obs1)      # for each agent... xun    
+            action2 = agent2.forward(obs2)      # for each agent... xun   
+            actions = [action1, action2]  
+             
+            reward1 = np.float32(0)
+            reward2 = np.float32(0)
+            done = False
+            
+           
+                
+            for _ in range(action_repetition):
+                observation, r, done, info = env.step(actions)   # combine and env.step
+                observation = deepcopy(observation)             # reward shall be separate
+                reward1 += r[0]     # for agent 1 and agent 2. xun   
+                reward2 += r[1]                                      
+                if done:
+                    break
+            if nb_max_episode_steps and episode_step >= nb_max_episode_steps - 1:
+                # Force a terminal state.
+                done = True
+
+            metrics1 = agent1.backward(reward1, terminal=done)       # The returned metrics are null
+            metrics2 = agent2.backward(reward2, terminal=done)
+            
+            episode_reward1 += reward1              # xun
+            episode_reward2 += reward2              # xun            
+            
+            
+            step_logs1 = {
+                'action': action1,
+                'observation': obs1,
+                'reward': reward1,
+                'metrics': metrics1,
+                'episode': episode,
+                'info': {}, # accumulated_info,
+            }
+            step_logs2 = {
+                'action': action2,
+                'observation': obs2,
+                'reward': reward2,
+                'metrics': metrics2,
+                'episode': episode,
+                'info': {}, # accumulated_info,
+            }
+            callbacks1.on_step_end(episode_step, step_logs1)
+            callbacks2.on_step_end(episode_step, step_logs2)
+            
+            
+            episode_step += 1
+            agent1.step += 1
+            agent2.step += 1
+
+            if done:
+                # This episode is finished, report and reset.
+                episode_logs1 = {
+                    'episode_reward': episode_reward1,
+                    'nb_episode_steps': episode_step,
+                    'nb_steps': agent1.step,
+                }
+                episode_logs2 = {
+                    'episode_reward': episode_reward2,
+                    'nb_episode_steps': episode_step,
+                    'nb_steps': agent2.step,
+                }                
+                
+                callbacks1.on_episode_end(episode, episode_logs1)
+                callbacks2.on_episode_end(episode, episode_logs2)
+                episode += 1
+                observation = None
+                episode_step = None
+                episode_reward = None
+                    
+    except KeyboardInterrupt:
+        did_abort = True
+    
+    callbacks1.on_train_end(logs={'did_abort': did_abort})
+    callbacks2.on_train_end(logs={'did_abort': did_abort})
+    
+    agent1._on_train_end()
+    agent2._on_train_end()
+    return history1, history2
+    
+
+agent1.load_weights('1CP_SARSA_weights.h5f') 
+agent2.load_weights('1CP_SARSA_weights.h5f') 
+# Use my own fit func to fit two agents together. Xun, Sep 2021
+Xun_fit2(agent1, agent2, env, nb_steps = 80000, verbose = 1)
+env.env.close()
+#pdb.set_trace()
+
+agent1.save_weights('2CP_SARSA_agent_4states_1.h5f', overwrite=True)
+agent2.save_weights('2CP_SARSA_agent_4states_2.h5f', overwrite=True)
 env.close()
+    
+
+
